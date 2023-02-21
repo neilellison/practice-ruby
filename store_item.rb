9b5b143f1f8f3cfbb23e5b3a30d05ba44d368a8a
NeilEllison <ellison.neil1@gmail.com>
2023-02-21 12:20:11 -0800

    Ruby practice

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+item1 = {:color => "blue", :price => "150", :size => "large"}
+item2 = {:color => "red", :price => "90", :size => "medium"}
+item3 = {:color => "green", :price => "60", :size => "small"}
+
+puts item1
