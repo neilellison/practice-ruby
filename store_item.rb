d3c45035f1ee949a1d2e2775488ca098dbf561d4
NeilEllison <ellison.neil1@gmail.com>
2023-02-22 11:17:29 -0800

    Ruby exercises

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,29 @@
-item1 = {:color => "blue", :price => "150", :size => "large"}
-item2 = {:color => "red", :price => "90", :size => "medium"}
-item3 = {:color => "green", :price => "60", :size => "small"}
+class Items
+  attr_accessor :color, :price, :size
 
-puts item1
+  def initialize(input_options)
+    @color = input_options[:color]
+    @price = input_options[:price]
+    @size = input_options[:size]
+  end
+
+  def print_info
+  puts "The item is #{@color}, it costs #{@price}, and it is a size #{@size}."
+  end
+
+  def sale_price
+  @price = 0.70 * @price
+  end  
+end
+
+item1 = Items.new({color: "red", price: 120, size: "large"})
+item2 = Items.new({color: "blue", price: 90, size: "medium"}) 
+item3 = Items.new({color: "green", price: 65, size: "small"})
+
+
+item1.sale_price
+item2.sale_price
+item3.sale_price
+item1.print_info
+item2.print_info
+item3.print_info
